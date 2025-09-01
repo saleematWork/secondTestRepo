de44b21945f3582d84b51401c915df6aae9380c1
Muhammad <Saleem.Muhammad@analog.com>
2025-09-01 08:59:07 +0100

    adding first python code

---
+++ mainCode1.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+print("Hello world!")
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
